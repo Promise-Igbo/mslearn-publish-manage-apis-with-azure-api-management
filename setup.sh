985925b609473d0201c67581d67d9b852cf2feca
Promise Igbo <100773978+Promise-Igbo@users.noreply.github.com>
2025-07-20 14:38:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,8 @@ git config --global user.name "$GIT_USERNAME"
 git config --global user.email "$GIT_EMAIL"
 
 
-RESOURCE_GROUP=$(az group list --query "[0].name" -o tsv)
+RESOURCE_GROUP="apimtrainingrg"
+LOCATION="eastus2"
 
 # Create App Service plan
 PLAN_NAME=myPlanapi
@@ -21,7 +22,7 @@ PLAN_NAME=myPlanapi
 printf "\nCreating App Service plan in FREE tier ... (2/7)\n\n"
 
 
-az appservice plan create --name $apiappname --resource-group $RESOURCE_GROUP --sku FREE --location eastus2 --verbose
+az appservice plan create --name $PLAN_NAME --resource-group $RESOURCE_GROUP --sku FREE --location $LOCATION --verbose
 
 printf "\nCreating API App ... (3/7)\n\n"
 
